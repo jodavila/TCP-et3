a5448ac8454433b8ee0d326c526d6afb5d9547e5
Joana DAvila <joanaoldavila@gmail.com>
2024-08-18 15:33:11 -0300

    Refactor SudokuMain class to improve code organization and readability

--- sukodu/Generator.java
+++ sukodu/Generator.java
@@ -1,8 +1,8 @@
 package sukodu;
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;;
-import java.net.http.HttpRequest;;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 /**
--- sukodu/SudokuMain.java
+++ sukodu/SudokuMain.java
@@ -5,17 +5,23 @@ import javax.swing.*;
  * The main Sudoku program
  */
 public class SudokuMain extends JFrame {
-   private static final long serialVersionUID = 1L;  // to prevent serial warning
+   private static final long serialVersionUID = 1L; 
    private JComboBox<String> difficultyComboBox;
-   Board board = new Board();
-   JButton btnNewGame = new JButton("New Game");
+   private Board board = new Board();
+   private JButton btnNewGame = new JButton("New Game");
 
-   // Constructor
    public SudokuMain() {
+      initUI();
+      pack();     
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+      setTitle("SuKoDu");
+      setVisible(true);
+   }
+
+   private void initUI() {
       Container cp = getContentPane();
       cp.setLayout(new BorderLayout());
 
-      // Add difficulty selection pane
       JPanel topPanel = new JPanel();
       difficultyComboBox = new JComboBox<>(new String[]{"easy", "medium", "hard"});
       topPanel.add(new JLabel("Select Difficulty:"));
@@ -23,27 +29,14 @@ public class SudokuMain extends JFrame {
       cp.add(topPanel, BorderLayout.NORTH);
       cp.add(board, BorderLayout.CENTER);
 
-      // Add a button to the south to re-start the game via board.newGame() with selected difficulty
       JPanel bottomPanel = new JPanel();
       bottomPanel.add(btnNewGame);
       cp.add(bottomPanel, BorderLayout.SOUTH);
 
-      pack();     // Pack the UI components, instead of using setSize()
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // to handle window-closing
-      setTitle("SuKoDu");
-      setVisible(true);
-
-      // Add trigger to initialize the game
       btnNewGame.addActionListener(e -> board.newGame(difficultyComboBox.getSelectedItem().toString()));
    }
 
-   // main method
    public static void main(String[] args) {
-      // Run GUI codes in the Event-Dispatching thread for thread safety
-      SwingUtilities.invokeLater(new Runnable() {
-         public void run() {
-            new SudokuMain();
-         }
-      });
+      SwingUtilities.invokeLater(() -> new SudokuMain());
    }
 }
